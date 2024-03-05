fd4d7e30ce4e178e2796b6805f1fe2659966b8eb
plievm <plievm000@gmail.com>
2024-03-05 12:33:10 +0300

    nachalo

--- 2.py
+++ 2.py
@@ -1,25 +1,15 @@
-def insertionSort(arr):
-    n = len(arr)
-    if n <= 1:
-        return
-    for i in range(1, n):
-        key = arr[i]
-        j = i - 1
-        while j >= 0 and key < arr[j]:
-            arr[j + 1] = arr[j]
-            j -= 1
-        arr[j + 1] = key
 f = open("students.csv", encoding="utf8")
 students = []
+f.readline()
 for i in f:
     students.append(i[:-1])
 xd = []
 a=[]
 class Student:
     pass
-for i in range(len(students)-1):
+for i in range(len(students)):
     xd.append(Student())
-for i in range(1,len(students)-1):
+for i in range(len(students)):
     s = students[i].split(",")
     xd[i].idd = s[0]
     xd[i].name = s[1]
@@ -28,10 +18,15 @@ for i in range(1,len(students)-1):
     if s[4] == "None":
         s[4] = '0'
     xd[i].score = s[4]
-    a.append(xd[i].score)
-print(insertionSort(a))
 print("10 класс:")
 k=0
+for i in range(len(xd)):
+    t = xd[i]
+    j = i - 1
+    while j >= 0 and t.score > xd[j].score:
+        xd[j+1] = xd[j]
+        j -= 1
+    xd[j+1] = t
 for i in range(1,len(students)):
     if xd[i].clas[:-1] == "10":
         k += 1
