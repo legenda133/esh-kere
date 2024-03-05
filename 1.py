82afbc3a229bb9dc076653d6ab0eb7fcd003f457
plievm <plievm000@gmail.com>
2024-03-05 12:06:58 +0300

    nachalo

--- 1.py
+++ 1.py
@@ -1,5 +1,6 @@
 f = open("students.csv", encoding="utf8")
 students = []
+name = f.readline().strip()
 for i in f:
     students.append(i[:-1])
 xd = []
@@ -14,10 +15,19 @@ for i in range(len(students)-1):
     xd[i].tidd = s[2]
     xd[i].clas = s[3]
     xd[i].score = s[4]
-    if s[4] == "None":
-        s[4] = -1
-
     if 'Хадаров Владимир' in xd[i].name:
          print('Ты получил:', xd[i].score,'за проект -', xd[i].tidd)
-    else:
-        continue
+for i in xd:
+    s = 0
+    k = 0
+    if i.score == 'None':
+        for j in xd:
+            if j.clas == i.clas and j.score != 'None':
+                k += 1
+                s += int(j.score)
+        i.score = f'{s / k:.3f}'
+f = open('students_new.csv', 'w')
+print(name, file=f)
+for i in xd:
+    print(i.idd, ',', i.name, ',', i.tidd, ',', i.clas, ',', i.score, file=f)
+f.close()
